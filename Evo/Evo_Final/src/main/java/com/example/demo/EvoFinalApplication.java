29de7584759bf1a0f6e55b3eb39288f556831ecb
ChristianDioses <70173082+ChristianDioses@users.noreply.github.com>
2021-11-29 01:52:00 -0500

    Add files via upload

---
+++ Evo/Evo_Final/src/main/java/com/example/demo/EvoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoFinalApplication.class, args);
+	}
+
+}
---
+++ Evo/Evo_Final/src/main/java/com/example/demo/Prueba2.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Prueba2 {
+
+	String nombre;
+	String apellido;
+}
